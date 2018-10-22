c654edb9407cb50a43821c7ee97fe032f57df218
Niles <niulinguo@163.com>
2018-10-22 22:20:55 +0800

    Initial commit

---
+++ app/src/main/java/com/niles/umengsharemodule/MyApp.java
@@ -0,0 +1,16 @@
+package com.niles.umengsharemodule;
+
+import android.app.Application;
+
+/**
+ * Created by Niles
+ * Date 2018/10/22 22:11
+ * Email niulinguo@163.com
+ */
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
